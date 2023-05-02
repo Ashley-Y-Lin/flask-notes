8e9cc8c9bcee0e6130a94cfb2c9be172bf7d5786
Ashley Lin <ashleyylin11@gmail.com>
2023-05-02 16:02:25 -0700

    add routes for users and notes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, session, redirect, render_template, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db, User
+from models import db, connect_db, User, Note
 
-from forms import AddNewUserForm, LoginUserForm, CSRFProtectForm
+from forms import (
+    AddNewUserForm,
+    LoginUserForm,
+    CSRFProtectForm,
+    AddNewNoteForm,
+    EditNoteForm,
+)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask_notes"
@@ -25,18 +31,19 @@ debug = DebugToolbarExtension(app)
 
 #### Authentication Routes ###
 
+
 @app.get("/")
 def redirect_register():
     """Redirect to /register."""
 
     return redirect("/register")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Produce register form or handle register."""
 
-
-    #Redirect if already logged in
+    # Redirect if already logged in
     if "username" in session:
         username = session["username"]
         return redirect(f"/users/{username}")
@@ -68,11 +75,12 @@ def register():
 
     return render_template("register.html", form=form)
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Produce login form or handle login."""
 
-    #Redirect if already logged in
+    # Redirect if already logged in
     if "username" in session:
         username = session["username"]
         return redirect(f"/users/{username}")
@@ -95,6 +103,7 @@ def login():
 
     return render_template("login.html", form=form)
 
+
 @app.post("/logout")
 def logout_user():
     """Log the user out and redirect to /."""
@@ -107,7 +116,9 @@ def logout_user():
 
     return redirect("/")
 
+
 ### User Routes ###
+
 
 @app.get("/users/<username>")
 def display_user_info(username):
@@ -115,15 +126,16 @@ def display_user_info(username):
     Ensure users are logged in to see the page, else redirect to /login."""
 
     if "username" not in session:
-        if session["username"] != username:
-            flash("Invalid credentials!")
-        else:
-            flash("You must be logged in to view!")
+        flash("You must be logged in to add a note!")
+        return redirect("/")
+    elif session["username"] != username:
+        flash("Invalid login credentials!")
         return redirect("/")
 
     user = User.query.get_or_404(username)
 
     return render_template("user-info.html", user=user, notes=user.notes)
+
 
 @app.post("/users/<username>/delete")
 def delete_user(username):
@@ -135,7 +147,7 @@ def delete_user(username):
         notes = user.notes
 
         for note in notes:
-            db.session.delete(notes)
+            db.session.delete(note)
 
         db.session.delete(user)
         db.session.commit()
@@ -146,6 +158,84 @@ def delete_user(username):
 
     return redirect("/")
 
+
 ### NOTES ROUTES ###
 
-@app.route("users/<username>/notes/add", methods)
+
+@app.route("/users/<username>/notes/add", methods=["GET", "POST"])
+def display_or_add_note(username):
+    """Produce add note form or handles add note form submission."""
+
+    if "username" not in session:
+        flash("You must be logged in to add a note!")
+        return redirect("/")
+    elif session["username"] != username:
+        flash("Invalid login credentials!")
+        return redirect("/")
+
+    form = AddNewNoteForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        note = Note(
+            title=title,
+            content=content,
+            owner_username=username,
+        )
+
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(f"/users/{username}")
+
+    return render_template("add-note.html", form=form)
+
+
+@app.route("/notes/<int:note_id>/update", methods=["GET", "POST"])
+def edit_or_update_note(note_id):
+    """Produce edit note form or handles edit note form submission."""
+
+    note = Note.query.get_or_404(note_id)
+
+    if "username" not in session:
+        flash("You must be logged in to edit a note!")
+        return redirect("/")
+    elif session["username"] != note.owner_username:
+        flash("Invalid login credentials!")
+        return redirect("/")
+
+    form = EditNoteForm()
+
+    if form.validate_on_submit():
+        note.title = form.title.data or note.title
+        note.content = form.content.data or note.content
+
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(f"/users/{note.owner_username}")
+
+    # pre-populating the form input
+    form.title.data = note.title
+    form.content.data = note.content
+
+    return render_template("edit-note.html", form=form, note=note)
+
+
+@app.post("/notes/<int:note_id>/delete")
+def delete_note(note_id):
+    """Deletes note from database and redirects to /users/<username>"""
+    form = CSRFProtectForm()
+
+    if form.validate_on_submit:
+        note = Note.query.get_or_404(note_id)
+        username = note.owner_username
+
+        db.session.delete(note)
+        db.session.commit()
+
+        flash(f"{note.title} was deleted!")
+
+    return redirect(f"/users/{username}")
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 """Forms for flask notes."""
 
-from wtforms import StringField, EmailField, PasswordField
+from wtforms import StringField, EmailField, PasswordField, TextAreaField
 from flask_wtf import FlaskForm
 
 from wtforms.validators import InputRequired, Length
@@ -33,6 +33,22 @@ class LoginUserForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(max=20)])
 
     password = PasswordField("Password", validators=[InputRequired(), Length(max=100)])
+
+
+class AddNewNoteForm(FlaskForm):
+    """Form for adding a new note."""
+
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
+
+    content = TextAreaField("Content", validators=[InputRequired()])
+
+
+class EditNoteForm(FlaskForm):
+    """Form for editing an existing note."""
+
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
+
+    content = TextAreaField("Content", validators=[InputRequired()])
 
 
 class CSRFProtectForm(FlaskForm):
